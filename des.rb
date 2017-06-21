c7f1b2ba2c94850bf1691fa851bd2b1bf6d5e0c6
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-22 00:16:53 +0900

    bin.install

--- des.rb
+++ des.rb
@@ -1,8 +1,9 @@
 class Des < Formula
   desc "CLI to create Docker Environment Setting files."
-  homepage ""
+  homepage "https://github.com/at-grandpa/des"
   url "https://github.com/at-grandpa/des/archive/v0.1.0.tar.gz"
   sha256 "8d93017fd35af280180ad29d1550f1c179d54f40bbc572e454afb90a10d5faf7"
+
 
   def install
     bin.install "des"
