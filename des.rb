4823a218742f1faa60e809e49b65137d378a1f73
at_grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-22 08:25:45 +0900

    Update des.rb

--- des.rb
+++ des.rb
@@ -2,7 +2,7 @@ class Des < Formula
   desc "CLI to create Docker Environment Setting files."
   homepage "https://github.com/at-grandpa/des"
   url "https://github.com/at-grandpa/des/releases/download/0.1.0/des-0.1.0-darwin-x86_64.tar.gz"
-  sha256 "1d7edc9071ff0086d67c9258da062367082488f4197295190b8b3ac5dfb718e9"
+  sha256 "209932027f75e392f3d5adb02140dd0495ef0649bdfaf871d8633ff35fd590e4"
 
   def install
     bin.install "des"
