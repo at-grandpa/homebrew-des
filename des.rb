6fb6d858cbf4b270ab8693a26be08d619134dff6
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-22 00:53:42 +0900

    ...

--- des.rb
+++ des.rb
@@ -4,9 +4,8 @@ class Des < Formula
   url "https://github.com/at-grandpa/des/releases/download/0.1.0/des-0.1.0-darwin-x86_64.tar.gz"
   sha256 "6b25679309e4b8a20ea26be6f255b69ec877ccae673db91c527e8e603b017fc7"
 
-
   def install
-    bin.install "hoge"
+    bin.install "des"
   end
 
   test do
