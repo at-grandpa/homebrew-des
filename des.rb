e75461e4934f8c9a5831aafab61a846f97876285
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-18 22:14:38 +0900

    crystal version up

--- des.rb
+++ des.rb
@@ -1,8 +1,8 @@
 class Des < Formula
   desc "CLI to create Docker Environment Setting files."
   homepage "https://github.com/at-grandpa/des"
-  url "https://github.com/at-grandpa/des/releases/download/0.1.2/des-0.1.2-darwin-x86_64.tar.gz"
-  sha256 "48ea1e9dfa136700d103e0da37a5afe6fe8f0adb5e9c6d4526e01469f4195da9"
+  url "https://github.com/at-grandpa/des/releases/download/0.1.3/des-0.1.3-darwin-x86_64.tar.gz"
+  sha256 "bbdf4a9d8a68a598e90263f9a4167163a9225bc50ddd8ffa4a9d0ef7c0407396"
 
   def install
     bin.install "des"
